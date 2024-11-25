f5b7366091ff80551e05bf6c6b38a8c476d113b5
Pauline Metay <metaypauline@gmail.com>
2024-11-25 12:25:57 +0100

    v2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,11 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @input = params[:ask]
-    if params[:ask]
-      if params[:ask].include?("I am going to work")
+    @input = params[:question]
+    if params[:question]
+      if params[:question].include?("I am going to work")
         @answer = "Great!"
-      elsif params[:ask].include?("?")
+      elsif params[:question].include?("?")
         @answer = "Silly question, get dressed and go to work!"
       else
         @answer = "I don't care, get dressed and go to work!"
